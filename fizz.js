00b0b20430385eb577c65c6fcb87f9ff86b99049
AndreaChiari <chiariandrea94@gmail.com>
2022-12-07 17:05:54 +0100

    update-struttura

--- fizz.js
+++ fizz.js
@@ -10,10 +10,18 @@ console.log('JS OK')
 for (let i = 1; i <= 100; i++){
    
 
-    if ( i % 3 === 0 );
+    if ( i % 3 === 0 ){ 
     console.log('Fizz');
+    }
 
-   
+    if ( i % 5 === 0 ){ 
+    console.log('Buzz');
+    }
+
+    if(i % 3 === 0 || i % 5 === 0 ){
+    console.log('Fizzbuzz');
+
+    }
     
    
     
