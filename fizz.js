90c22a05a31aa7a029240a6c527437bde95b5d1f
AndreaChiari <chiariandrea94@gmail.com>
2022-12-07 15:51:40 +0100

    add-boiler-plate

---
+++ fizz.js
@@ -0,0 +1,11 @@
+console.log('JS OK')
+/*
+1)imposto un ciclo di for per generare i numeri da 1 a 100
+2)applico delle condizioni per filtrare i multipli di 3
+3)applico delle condizioni per filtrare i multipli di 5
+4)applico delle condizioni affiche' i multipli di 3 e 5 cengano raggruppate 
+5)applico delle classi ad ogni condizione
+6)stampo in pagina */
+
+for (let i = 1; i <= 100; i++);
+console.log(i);
