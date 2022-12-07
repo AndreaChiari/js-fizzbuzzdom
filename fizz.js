f48fb275b0138e00572f231a8fb9416c82e7ea06
AndreaChiari <chiariandrea94@gmail.com>
2022-12-07 17:24:13 +0100

    update

--- fizz.js
+++ fizz.js
@@ -14,10 +14,19 @@ for (let i = 1; i <= 100; i++){
     console.log('Fizz');
     }
 
-    else if( i % 5 === 0 ) 
+    if ( i % 5 === 0 ){ 
     console.log('Buzz');
+    }
 
-    else( i % 3 === 0 || i % 5 === 0)
+    if ( i % 3 === 0 || i % 5 === 0){
+    console.log('Fizzbuzz');
+    }
+
+    else {
+        console.log(i)
+    }
+
+   
     
 
    
